3b93455f45cf4f70c46f0f7eb9d3dabbbd9b01ea
mnatasyar <mnatasyar0@gmail.com>
2024-05-30 18:55:16 +0700

    add styled component

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
 import RestaurantsList from "../components/RestaurantsList";
+import Container from "../components/styled/Container";
+
 export default function Home({ restaurants }) {
   return (
-    <>
-      <div className="container">
-        <RestaurantsList restaurants={restaurants} />
-      </div>
-    </>
+    <Container maxWidth="80%">
+      <RestaurantsList restaurants={restaurants} />
+    </Container>
   );
 }
+
 export async function getStaticProps() {
   const res = await fetch("https://restaurant-api.dicoding.dev/list");
   const { restaurants } = await res.json();
